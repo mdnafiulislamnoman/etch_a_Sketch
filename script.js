90d5d3a3d6b0083764e6b4cd29843dd890d0ae7d
mdnafiulislamnoman <software.engr.nafiul@gmail.com>
2025-07-17 01:31:28 +0600

    Used css styles to style those grids. Used DOM for creating grids and event listener for hover effect.

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector("#container");
+
+for(i = 0; i < 256; i++){
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+
+    square.addEventListener('mouseover',() => {
+        square.style.backgroundColor = "black";
+    })
+}
+
